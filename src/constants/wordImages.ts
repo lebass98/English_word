264d0d886f583c5b02413c8d2b33bed00a7442a8
dongbookro <dongbookro@gmail.com>
2026-09-14 07:57:13 +0900

    feat: 토플 2단원 acquisition (획득, 취득, 습득; 매입) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImages.ts
+++ src/constants/wordImages.ts
@@ -55,6 +55,7 @@ export const WORD_IMAGES: Record<string, ImageSourcePropType> = {
   ache: require("../../assets/words/ache.png"),
   achieve: require("../../assets/words/achieve.png"),
   acquire: require("../../assets/words/acquire.png"),
+  acquisition: require("../../assets/words/acquisition.png"),
   activity: require("../../assets/words/activity.png"),
   actually: require("../../assets/words/actually.png"),
   add: require("../../assets/words/add.png"),

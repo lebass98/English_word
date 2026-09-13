2b7b4c46fc401725d23dc555dc2e1095966aee08
dongbookro <dongbookro@gmail.com>
2026-09-14 07:54:34 +0900

    feat: 토플 2단원 acquire (얻다, 획득하다, 습득하다) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImages.ts
+++ src/constants/wordImages.ts
@@ -54,6 +54,7 @@ export const WORD_IMAGES: Record<string, ImageSourcePropType> = {
   "accustomed to": require("../../assets/words/accustomed-to.png"),
   ache: require("../../assets/words/ache.png"),
   achieve: require("../../assets/words/achieve.png"),
+  acquire: require("../../assets/words/acquire.png"),
   activity: require("../../assets/words/activity.png"),
   actually: require("../../assets/words/actually.png"),
   add: require("../../assets/words/add.png"),

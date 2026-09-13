62c97fea8e9cbb9ed41d7b90ea56717b426fdb17
dongbookro <dongbookro@gmail.com>
2026-09-14 07:43:33 +0900

    feat: 토플 2단원 accuracy (정확성, 정밀도) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImages.ts
+++ src/constants/wordImages.ts
@@ -49,6 +49,7 @@ export const WORD_IMAGES: Record<string, ImageSourcePropType> = {
   account: require("../../assets/words/account.png"),
   "account for": require("../../assets/words/account-for.png"),
   accumulate: require("../../assets/words/accumulate.png"),
+  accuracy: require("../../assets/words/accuracy.png"),
   ache: require("../../assets/words/ache.png"),
   activity: require("../../assets/words/activity.png"),
   actually: require("../../assets/words/actually.png"),

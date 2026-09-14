6206910b5f21baf26ea13acad5e545740e932a27
dongbookro <dongbookro@gmail.com>
2026-09-14 11:30:08 +0900

    feat: 토플 5단원 antique (골동품의, 오래된; 골동품) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImages.ts
+++ src/constants/wordImages.ts
@@ -128,6 +128,7 @@ export const WORD_IMAGES: Record<string, ImageSourcePropType> = {
   answer: require("../../assets/words/answer.png"),
   antecedent: require("../../assets/words/antecedent.png"),
   anticipate: require("../../assets/words/anticipate.png"),
+  antique: require("../../assets/words/antique.png"),
   anxious: require("../../assets/words/anxious.png"),
   anyway: require("../../assets/words/anyway.png"),
   apart: require("../../assets/words/apart.png"),

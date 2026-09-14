2b4f79712066b5f011223e12236a44ccdbf13e5a
dongbookro <dongbookro@gmail.com>
2026-09-14 14:53:00 +0900

    feat: 토플 9단원 barter (물물 교환하다; 물물 교환) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImagesByLetter/b.ts
+++ src/constants/wordImagesByLetter/b.ts
@@ -13,6 +13,7 @@ export const IMAGES_B: Record<string, ImageSourcePropType> = {
   barely: require("../../../assets/words/b/barely.png"),
   barren: require("../../../assets/words/b/barren.png"),
   barrier: require("../../../assets/words/b/barrier.png"),
+  barter: require("../../../assets/words/b/barter.png"),
   base: require("../../../assets/words/b/base.png"),
   basement: require("../../../assets/words/b/basement.png"),
   bathroom: require("../../../assets/words/b/bathroom.png"),

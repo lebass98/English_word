3eb0bbdd7db0e1b1133ecf40b9b235d902f567b3
dongbookro <dongbookro@gmail.com>
2026-09-14 15:36:56 +0900

    feat: 토플 10단원 benediction (축복, 축복 기도) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImagesByLetter/b.ts
+++ src/constants/wordImagesByLetter/b.ts
@@ -52,6 +52,7 @@ export const IMAGES_B: Record<string, ImageSourcePropType> = {
   belt: require("../../../assets/words/b/belt.png"),
   bend: require("../../../assets/words/b/bend.png"),
   beneath: require("../../../assets/words/b/beneath.png"),
+  benediction: require("../../../assets/words/b/benediction.png"),
   besides: require("../../../assets/words/b/besides.png"),
   bet: require("../../../assets/words/b/bet.png"),
   beyond: require("../../../assets/words/b/beyond.png"),

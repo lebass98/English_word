e57fba532727e122ce5b3a81b94fa198c1be1a11
dongbookro <dongbookro@gmail.com>
2026-09-14 15:31:16 +0900

    feat: 토플 10단원 beings (생물, 존재) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImagesByLetter/b.ts
+++ src/constants/wordImagesByLetter/b.ts
@@ -45,6 +45,7 @@ export const IMAGES_B: Record<string, ImageSourcePropType> = {
   behavior: require("../../../assets/words/b/behavior.png"),
   behind: require("../../../assets/words/b/behind.png"),
   behold: require("../../../assets/words/b/behold.png"),
+  beings: require("../../../assets/words/b/beings.png"),
   believe: require("../../../assets/words/b/believe.png"),
   belong: require("../../../assets/words/b/belong.png"),
   below: require("../../../assets/words/b/below.png"),

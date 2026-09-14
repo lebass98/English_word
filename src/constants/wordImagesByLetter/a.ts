486d294c075d07611604b857f5aa627a531ff7f1
dongbookro <dongbookro@gmail.com>
2026-09-14 14:22:06 +0900

    feat: 토플 8단원 avoid (피하다, 회피하다) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImagesByLetter/a.ts
+++ src/constants/wordImagesByLetter/a.ts
@@ -197,4 +197,5 @@ export const IMAGES_A: Record<string, ImageSourcePropType> = {
   authenticity: require("../../../assets/words/a/authenticity.png"),
   authorize: require("../../../assets/words/a/authorize.png"),
   available: require("../../../assets/words/a/available.png"),
+  avoid: require("../../../assets/words/a/avoid.png"),
 };

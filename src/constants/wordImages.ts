4d6479945bb327b931a86a809cf50039dbed6ab4
이재광 <yellow@wordncode.com>
2026-09-14 12:52:01 +0900

    feat: 고등학교 2학년 단어 biodiversity 선형그래픽 이미지 추가 및 등록

--- src/constants/wordImages.ts
+++ src/constants/wordImages.ts
@@ -145,6 +145,7 @@ export const WORD_IMAGES: Record<string, ImageSourcePropType> = {
   bike: require("../../assets/words/bike.png"),
   bill: require("../../assets/words/bill.png"),
   biology: require("../../assets/words/biology.png"),
+  biodiversity: require("../../assets/words/biodiversity.png"),
   birth: require("../../assets/words/birth.png"),
   bit: require("../../assets/words/bit.png"),
   bite: require("../../assets/words/bite.png"),

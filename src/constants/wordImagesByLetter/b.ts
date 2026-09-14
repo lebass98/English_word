9d17e8f0d3afa577a2e89d07757b5d9b989179ef
dongbookro <dongbookro@gmail.com>
2026-09-14 14:41:09 +0900

    feat: 토플 9단원 barely (간신히, 거의 ~ 않다) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImagesByLetter/b.ts
+++ src/constants/wordImagesByLetter/b.ts
@@ -9,6 +9,7 @@ export const IMAGES_B: Record<string, ImageSourcePropType> = {
   band: require("../../../assets/words/b/band.png"),
   bar: require("../../../assets/words/b/bar.png"),
   bare: require("../../../assets/words/b/bare.png"),
+  barely: require("../../../assets/words/b/barely.png"),
   base: require("../../../assets/words/b/base.png"),
   basement: require("../../../assets/words/b/basement.png"),
   bathroom: require("../../../assets/words/b/bathroom.png"),

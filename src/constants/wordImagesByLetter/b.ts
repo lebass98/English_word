7c1686e35a3b2a6eab5c121bd2242bd31ea4e729
dongbookro <dongbookro@gmail.com>
2026-09-14 16:00:12 +0900

    feat: 토플 10단원 blossom (번성하다, 발전하다; 꽃이 피다; 꽃) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImagesByLetter/b.ts
+++ src/constants/wordImagesByLetter/b.ts
@@ -74,6 +74,7 @@ export const IMAGES_B: Record<string, ImageSourcePropType> = {
   blind: require("../../../assets/words/b/blind.png"),
   block: require("../../../assets/words/b/block.png"),
   blood: require("../../../assets/words/b/blood.png"),
+  blossom: require("../../../assets/words/b/blossom.png"),
   blow: require("../../../assets/words/b/blow.png"),
   board: require("../../../assets/words/b/board.png"),
   body: require("../../../assets/words/b/body.png"),

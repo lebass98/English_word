579eefc13aaecf8a390a8d250150c67610088b8e
dongbookro <dongbookro@gmail.com>
2026-09-14 11:16:34 +0900

    feat: 토플 5단원 amplify (증폭시키다, 확대하다, 늘리다) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImages.ts
+++ src/constants/wordImages.ts
@@ -117,6 +117,7 @@ export const WORD_IMAGES: Record<string, ImageSourcePropType> = {
   ambition: require("../../assets/words/ambition.png"),
   amount: require("../../assets/words/amount.png"),
   ample: require("../../assets/words/ample.png"),
+  amplify: require("../../assets/words/amplify.png"),
   amuse: require("../../assets/words/amuse.png"),
   ancestor: require("../../assets/words/ancestor.png"),
   ancient: require("../../assets/words/ancient.png"),

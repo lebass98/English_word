600ba3aa02d2d7471cff16f820f738ebc4d0e40e
dongbookro <dongbookro@gmail.com>
2026-09-14 13:33:02 +0900

    feat: 토플 7단원 assuredly (확실히, 틀림없이) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImages.ts
+++ src/constants/wordImages.ts
@@ -185,6 +185,7 @@ export const WORD_IMAGES: Record<string, ImageSourcePropType> = {
   assortment: require("../../assets/words/assortment.png"),
   assume: require("../../assets/words/assume.png"),
   assumption: require("../../assets/words/assumption.png"),
+  assuredly: require("../../assets/words/assuredly.png"),
   astonish: require("../../assets/words/astonish.png"),
   athlete: require("../../assets/words/athlete.png"),
   atom: require("../../assets/words/atom.png"),

66dd31024356da793506bf6d59f4d165252ca93a
dongbookro <dongbookro@gmail.com>
2026-09-14 12:01:11 +0900

    feat: 토플 6단원 appreciate (진가를 알아보다, 제대로 평가하다; 감사하다) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImages.ts
+++ src/constants/wordImages.ts
@@ -145,6 +145,7 @@ export const WORD_IMAGES: Record<string, ImageSourcePropType> = {
   application: require("../../assets/words/application.png"),
   appointment: require("../../assets/words/appointment.png"),
   appreciable: require("../../assets/words/appreciable.png"),
+  appreciate: require("../../assets/words/appreciate.png"),
   area: require("../../assets/words/area.png"),
   arise: require("../../assets/words/arise.png"),
   army: require("../../assets/words/army.png"),

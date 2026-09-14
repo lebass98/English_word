ed3de702a9c33890c55b309edd11e0cf5c660154
dongbookro <dongbookro@gmail.com>
2026-09-14 13:13:59 +0900

    feat: 토플 7단원 assign (지정하다, 배정하다, 할당하다) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImages.ts
+++ src/constants/wordImages.ts
@@ -178,6 +178,7 @@ export const WORD_IMAGES: Record<string, ImageSourcePropType> = {
   assemble: require("../../assets/words/assemble.png"),
   assert: require("../../assets/words/assert.png"),
   assertion: require("../../assets/words/assertion.png"),
+  assign: require("../../assets/words/assign.png"),
   astonish: require("../../assets/words/astonish.png"),
   athlete: require("../../assets/words/athlete.png"),
   atom: require("../../assets/words/atom.png"),

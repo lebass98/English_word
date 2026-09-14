adff1c859f3e6d62061dd718768ace9227a2c9a4
dongbookro <dongbookro@gmail.com>
2026-09-14 10:17:31 +0900

    feat: 토플 3단원 advisor (고문, 조언자, 상담역) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImages.ts
+++ src/constants/wordImages.ts
@@ -79,6 +79,7 @@ export const WORD_IMAGES: Record<string, ImageSourcePropType> = {
   adventure: require("../../assets/words/adventure.png"),
   adversary: require("../../assets/words/adversary.png"),
   advice: require("../../assets/words/advice.png"),
+  advisor: require("../../assets/words/advisor.png"),
   affair: require("../../assets/words/affair.png"),
   afraid: require("../../assets/words/afraid.png"),
   afterward: require("../../assets/words/afterward.png"),

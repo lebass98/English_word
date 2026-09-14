f5b875c435308d7831b0accad19f90bb5437b960
dongbookro <dongbookro@gmail.com>
2026-09-14 14:30:23 +0900

    feat: 토플 8단원 award ((상 등을) 수여하다; 상) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImagesByLetter/a.ts
+++ src/constants/wordImagesByLetter/a.ts
@@ -201,4 +201,5 @@ export const IMAGES_A: Record<string, ImageSourcePropType> = {
   authorize: require("../../../assets/words/a/authorize.png"),
   available: require("../../../assets/words/a/available.png"),
   avoid: require("../../../assets/words/a/avoid.png"),
+  award: require("../../../assets/words/a/award.png"),
 };

5b91c6dbd3568cdc14b73f2a73f19af4d10d7f77
dongbookro <dongbookro@gmail.com>
2026-09-14 15:17:31 +0900

    feat: 토플 9단원 be marked (두드러지다, 특징지어지다) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImagesByLetter/b.ts
+++ src/constants/wordImagesByLetter/b.ts
@@ -26,6 +26,7 @@ export const IMAGES_B: Record<string, ImageSourcePropType> = {
   "be considered": require("../../../assets/words/b/be-considered.png"),
   "be derived from": require("../../../assets/words/b/be-derived-from.png"),
   "be fond of": require("../../../assets/words/b/be-fond-of.png"),
+  "be marked": require("../../../assets/words/b/be-marked.png"),
   beach: require("../../../assets/words/b/beach.png"),
   bean: require("../../../assets/words/b/bean.png"),
   bear: require("../../../assets/words/b/bear.png"),

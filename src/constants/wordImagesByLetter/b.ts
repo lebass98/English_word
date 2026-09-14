53651a29ed36d8e46834f81e978bcb6730635d67
dongbookro <dongbookro@gmail.com>
2026-09-14 15:05:21 +0900

    feat: 토플 9단원 be characterized by (~이 특징이다, ~으로 특징지어지다) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImagesByLetter/b.ts
+++ src/constants/wordImagesByLetter/b.ts
@@ -22,6 +22,7 @@ export const IMAGES_B: Record<string, ImageSourcePropType> = {
   "be accustomed to": require("../../../assets/words/b/be-accustomed-to.png"),
   "be bound to": require("../../../assets/words/b/be-bound-to.png"),
   "be capable of": require("../../../assets/words/b/be-capable-of.png"),
+  "be characterized by": require("../../../assets/words/b/be-characterized-by.png"),
   beach: require("../../../assets/words/b/beach.png"),
   bean: require("../../../assets/words/b/bean.png"),
   bear: require("../../../assets/words/b/bear.png"),

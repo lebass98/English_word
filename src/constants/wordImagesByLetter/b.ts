7ddd63b3c1bc5e8c25707b024f2663dcc3d08d22
dongbookro <dongbookro@gmail.com>
2026-09-14 15:08:12 +0900

    feat: 토플 9단원 be considered (~으로 여겨지다) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImagesByLetter/b.ts
+++ src/constants/wordImagesByLetter/b.ts
@@ -23,6 +23,7 @@ export const IMAGES_B: Record<string, ImageSourcePropType> = {
   "be bound to": require("../../../assets/words/b/be-bound-to.png"),
   "be capable of": require("../../../assets/words/b/be-capable-of.png"),
   "be characterized by": require("../../../assets/words/b/be-characterized-by.png"),
+  "be considered": require("../../../assets/words/b/be-considered.png"),
   beach: require("../../../assets/words/b/beach.png"),
   bean: require("../../../assets/words/b/bean.png"),
   bear: require("../../../assets/words/b/bear.png"),

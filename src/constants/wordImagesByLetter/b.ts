bfacf7dc88d6b77b5da14185e3fac55c7abb2f02
dongbookro <dongbookro@gmail.com>
2026-09-14 16:34:00 +0900

    feat: 토플 11단원 brilliance (광채, 눈부심; 탁월함) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImagesByLetter/b.ts
+++ src/constants/wordImagesByLetter/b.ts
@@ -106,6 +106,7 @@ export const IMAGES_B: Record<string, ImageSourcePropType> = {
   bridge: require("../../../assets/words/b/bridge.png"),
   bright: require("../../../assets/words/b/bright.png"),
   brightness: require("../../../assets/words/b/brightness.png"),
+  brilliance: require("../../../assets/words/b/brilliance.png"),
   bring: require("../../../assets/words/b/bring.png"),
   british: require("../../../assets/words/b/british.png"),
   brown: require("../../../assets/words/b/brown.png"),

f0f2f2fcb17658ba9e12edff2231eb728e51cd92
dongbookro <dongbookro@gmail.com>
2026-09-14 11:03:05 +0900

    feat: 토플 4단원 alternative (대안, 선택 가능한 것; 대체의) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImages.ts
+++ src/constants/wordImages.ts
@@ -108,6 +108,7 @@ export const WORD_IMAGES: Record<string, ImageSourcePropType> = {
   alter: require("../../assets/words/alter.png"),
   alteration: require("../../assets/words/alteration.png"),
   alternate: require("../../assets/words/alternate.png"),
+  alternative: require("../../assets/words/alternative.png"),
   altogether: require("../../assets/words/altogether.png"),
   amateur: require("../../assets/words/amateur.png"),
   amazing: require("../../assets/words/amazing.png"),

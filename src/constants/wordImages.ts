48877593f43bdc0f6e2fc67a1f329232e2563b47
dongbookro <dongbookro@gmail.com>
2026-09-14 10:29:58 +0900

    feat: 토플 4단원 affluent (풍부한; 부유한) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImages.ts
+++ src/constants/wordImages.ts
@@ -84,6 +84,7 @@ export const WORD_IMAGES: Record<string, ImageSourcePropType> = {
   affair: require("../../assets/words/affair.png"),
   affection: require("../../assets/words/affection.png"),
   affluence: require("../../assets/words/affluence.png"),
+  affluent: require("../../assets/words/affluent.png"),
   afraid: require("../../assets/words/afraid.png"),
   afterward: require("../../assets/words/afterward.png"),
   agree: require("../../assets/words/agree.png"),

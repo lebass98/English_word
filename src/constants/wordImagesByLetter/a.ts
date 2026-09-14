159de3551324d73f031aca1500b10f4a59bf26e4
dongbookro <dongbookro@gmail.com>
2026-09-14 14:33:06 +0900

    feat: 토플 8단원 awareness (인식, 자각, 의식) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImagesByLetter/a.ts
+++ src/constants/wordImagesByLetter/a.ts
@@ -202,4 +202,5 @@ export const IMAGES_A: Record<string, ImageSourcePropType> = {
   available: require("../../../assets/words/a/available.png"),
   avoid: require("../../../assets/words/a/avoid.png"),
   award: require("../../../assets/words/a/award.png"),
+  awareness: require("../../../assets/words/a/awareness.png"),
 };

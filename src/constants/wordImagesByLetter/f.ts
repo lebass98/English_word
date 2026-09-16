e19a993ed932c772edf346783205c633d8445e99
dongbookro <dongbookro@gmail.com>
2026-09-16 16:40:59 +0900

    feat: 토익 6단원 fluctuate (변동하다) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImagesByLetter/f.ts
+++ src/constants/wordImagesByLetter/f.ts
@@ -87,6 +87,7 @@ export const IMAGES_F: Record<string, ImageSourcePropType> = {
   flood: require("../../../assets/words/f/flood.png"),
   flourish: require("../../../assets/words/f/flourish.png"),
   flow: require("../../../assets/words/f/flow.png"),
+  fluctuate: require("../../../assets/words/f/fluctuate.png"),
   fluent: require("../../../assets/words/f/fluent.png"),
   fluid: require("../../../assets/words/f/fluid.png"),
   flush: require("../../../assets/words/f/flush.png"),

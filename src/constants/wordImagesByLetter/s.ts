ad65f3338f052c560cfc68bec270855a29770eb5
dongbookro <dongbookro@gmail.com>
2026-09-16 17:07:28 +0900

    feat: 토익 9단원 subscribe (정기 구독을 신청하다 / 서비스에 가입하다) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImagesByLetter/s.ts
+++ src/constants/wordImagesByLetter/s.ts
@@ -251,6 +251,7 @@ export const IMAGES_S: Record<string, ImageSourcePropType> = {
   subconscious: require("../../../assets/words/s/subconscious.png"),
   subject: require("../../../assets/words/s/subject.png"),
   submit: require("../../../assets/words/s/submit.png"),
+  subscribe: require("../../../assets/words/s/subscribe.png"),
   subsequently: require("../../../assets/words/s/subsequently.png"),
   substance: require("../../../assets/words/s/substance.png"),
   substitute: require("../../../assets/words/s/substitute.png"),

0916c2cdc1a56e500b43b5aab2f54fb242765b31
dongbookro <dongbookro@gmail.com>
2026-09-16 16:21:49 +0900

    feat: 토익 4단원 reliable (믿을 수 있는, 신뢰할 수 있는) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImagesByLetter/r.ts
+++ src/constants/wordImagesByLetter/r.ts
@@ -66,6 +66,7 @@ export const IMAGES_R: Record<string, ImageSourcePropType> = {
   relay: require("../../../assets/words/r/relay.png"),
   release: require("../../../assets/words/r/release.png"),
   relevant: require("../../../assets/words/r/relevant.png"),
+  reliable: require("../../../assets/words/r/reliable.png"),
   relieve: require("../../../assets/words/r/relieve.png"),
   religion: require("../../../assets/words/r/religion.png"),
   religious: require("../../../assets/words/r/religious.png"),

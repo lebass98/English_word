72c8caf3aa62b59f0776610c9a9d21a06c3d0abf
dongbookro <dongbookro@gmail.com>
2026-09-16 18:00:28 +0900

    feat: 토익 13단원 commensurate (상응하는, 비례하는 / 상당하는) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImagesByLetter/c.ts
+++ src/constants/wordImagesByLetter/c.ts
@@ -152,6 +152,7 @@ export const IMAGES_C: Record<string, ImageSourcePropType> = {
   comic: require("../../../assets/words/c/comic.png"),
   command: require("../../../assets/words/c/command.png"),
   commence: require("../../../assets/words/c/commence.png"),
+  commensurate: require("../../../assets/words/c/commensurate.png"),
   comment: require("../../../assets/words/c/comment.png"),
   commerce: require("../../../assets/words/c/commerce.png"),
   commercial: require("../../../assets/words/c/commercial.png"),

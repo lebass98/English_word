8998399082dd4dd7d6148f6847a76e7aa37874b1
dongbookro <dongbookro@gmail.com>
2026-09-16 17:08:43 +0900

    feat: 토익 9단원 coverage (보상 범위 / 보도 범위) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImagesByLetter/c.ts
+++ src/constants/wordImagesByLetter/c.ts
@@ -304,6 +304,7 @@ export const IMAGES_C: Record<string, ImageSourcePropType> = {
   course: require("../../../assets/words/c/course.png"),
   cousin: require("../../../assets/words/c/cousin.png"),
   cover: require("../../../assets/words/c/cover.png"),
+  coverage: require("../../../assets/words/c/coverage.png"),
   crack: require("../../../assets/words/c/crack.png"),
   craft: require("../../../assets/words/c/craft.png"),
   crane: require("../../../assets/words/c/crane.png"),

66456edaa9a2cb0c73609a21da675a31858d5862
dongbookro <dongbookro@gmail.com>
2026-09-16 16:11:31 +0900

    feat: 토익 4단원 reimbursement (환급, 상환 / 보상) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImagesByLetter/r.ts
+++ src/constants/wordImagesByLetter/r.ts
@@ -52,6 +52,7 @@ export const IMAGES_R: Record<string, ImageSourcePropType> = {
   regret: require("../../../assets/words/r/regret.png"),
   regular: require("../../../assets/words/r/regular.png"),
   regulate: require("../../../assets/words/r/regulate.png"),
+  reimbursement: require("../../../assets/words/r/reimbursement.png"),
   rein: require("../../../assets/words/r/rein.png"),
   reinforce: require("../../../assets/words/r/reinforce.png"),
   reject: require("../../../assets/words/r/reject.png"),

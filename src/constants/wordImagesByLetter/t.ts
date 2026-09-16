bc7d212ae2f1b17cc1f60bf6a39726b626b49563
dongbookro <dongbookro@gmail.com>
2026-09-16 18:07:35 +0900

    feat: 토익 14단원 terminate (끝내다 / 해고하다) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImagesByLetter/t.ts
+++ src/constants/wordImagesByLetter/t.ts
@@ -32,6 +32,7 @@ export const IMAGES_T: Record<string, ImageSourcePropType> = {
   tentative: require("../../../assets/words/t/tentative.png"),
   tenure: require("../../../assets/words/t/tenure.png"),
   term: require("../../../assets/words/t/term.png"),
+  terminate: require("../../../assets/words/t/terminate.png"),
   terrible: require("../../../assets/words/t/terrible.png"),
   terrific: require("../../../assets/words/t/terrific.png"),
   terrifying: require("../../../assets/words/t/terrifying.png"),

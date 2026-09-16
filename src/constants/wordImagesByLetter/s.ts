94ea3d07c446c69b5157d2803cde03330ee05fbe
dongbookro <dongbookro@gmail.com>
2026-09-16 18:01:41 +0900

    feat: 토익 13단원 streamline (간소화하다, 효율화하다) 선형그래픽 이미지 생성 및 등록

--- src/constants/wordImagesByLetter/s.ts
+++ src/constants/wordImagesByLetter/s.ts
@@ -240,6 +240,7 @@ export const IMAGES_S: Record<string, ImageSourcePropType> = {
   strange: require("../../../assets/words/s/strange.png"),
   strategy: require("../../../assets/words/s/strategy.png"),
   stream: require("../../../assets/words/s/stream.png"),
+  streamline: require("../../../assets/words/s/streamline.png"),
   strengthen: require("../../../assets/words/s/strengthen.png"),
   stress: require("../../../assets/words/s/stress.png"),
   stretch: require("../../../assets/words/s/stretch.png"),

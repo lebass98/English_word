574029325b5ddf727d5eb57cba4429a5cc835646
lebass98 <132915660+lebass98@users.noreply.github.com>
2026-09-21 12:22:39 +0900

    feat: 일본어 JLPT N5 Unit 22 선형그래픽 이미지 제작 및 등록

--- src/constants/wordImagesByLetter/b.ts
+++ src/constants/wordImagesByLetter/b.ts
@@ -50,6 +50,7 @@ export const IMAGES_B: Record<string, ImageSourcePropType> = {
   "be derived from": require("../../../assets/words/b/be-derived-from.png"),
   "be fond of": require("../../../assets/words/b/be-fond-of.png"),
   "be known as": require("../../../assets/words/b/be-known-as.png"),
+  "be late": require("../../../assets/words/b/be-late.png"),
   "be likely to": require("../../../assets/words/b/be-likely-to.png"),
   "be marked": require("../../../assets/words/b/be-marked.png"),
   "be supposed to": require("../../../assets/words/b/be-supposed-to.png"),
